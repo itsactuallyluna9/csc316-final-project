dacb8d9ffe3b099737e7618988b1377275676a9a
Luna <itsactuallyluna9@gmail.com>
2025-09-11 22:08:34 -0500

    add basic (hopefully working) mod template

---
+++ hk-mods/SuperFancyInteropMod/SuperFancyInteropMod.cs
@@ -0,0 +1,31 @@
+﻿using Modding;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using UObject = UnityEngine.Object;
+
+namespace SuperFancyInteropMod
+{
+    internal class SuperFancyInteropMod : Mod
+    {
+        internal static SuperFancyInteropMod Instance { get; private set; }
+
+        public SuperFancyInteropMod() : base("Super Fancy Interop Mod") { }
+
+        public override string GetVersion()
+        {
+            return Assembly.GetExecutingAssembly().GetName().Version.ToString();
+        }
+
+        public override void Initialize()
+        {
+            Log("Initializing");
+
+            Instance = this;
+
+            Log("Initialized");
+        }
+    }
+}
