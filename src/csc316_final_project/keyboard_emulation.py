4e7dc0ebfe1f8150ed649341009de3d2dba742c0
Ryan Jacobs <155581268+arjay464@users.noreply.github.com>
2025-09-13 14:04:32 -0500

    Add files via upload

---
+++ src/csc316_final_project/keyboard_emulation.py
@@ -0,0 +1,82 @@
+import time
+from pynput import keyboard
+from pynput.keyboard import Key
+
+class HollowKnightController:
+    def __init__(self):
+        self.kb = keyboard.Controller()
+        self.pressed_keys = set()
+        
+        # Hollow Knight key mappings
+        self.key_map = {
+            'jump': 'z',           # Jump
+            'attack': 'x',         # Attack
+            'focus': 'a',         # Focus/Heal
+            'left': Key.left,     # Move left
+            'right': Key.right,   # Move right
+        }
+        
+        self.running = True
+        
+    def press_key(self, action):
+        if action in self.key_map:
+            key = self.key_map[action]
+            if key not in self.pressed_keys:
+                self.kb.press(key)
+                self.pressed_keys.add(key)
+                print(f"Pressed: {action}")
+    
+    def release_key(self, action):
+        if action in self.key_map:
+            key = self.key_map[action]
+            if key in self.pressed_keys:
+                self.kb.release(key)
+                self.pressed_keys.discard(key)
+                print(f"Released: {action}")
+    
+    def release_all(self):
+        for key in self.pressed_keys.copy():
+            self.kb.release(key)
+        self.pressed_keys.clear()
+        print("Released all keys")
+    
+    def neural_network_output(self, nn_output: dict): #nn_output expects a dictionary of booleans
+        # Press keys based on NN output
+        for action, value in nn_output.items():
+            if value: 
+                self.press_key(action)
+            else:
+                self.release_key(action)
+    
+    def stop(self):
+        self.release_all()
+        self.running = False
+
+
+if __name__ == "__main__":
+    controller = HollowKnightController()
+    
+    print("Hollow Knight Controller Ready!")
+    print("Test initializing in 3 seconds.")
+    time.sleep(3)
+    
+
+    
+    try:
+        print("\n=== Testing Basic Movement ===")
+        controller.neural_network_output({
+            "jump" : True,
+            "left" : True,
+            "right": False,
+            "attack" : False,
+            "focus" : False
+        })
+        
+        print("\n=== Testing Neural Network Input Simulation ===")
+        # Simulate neural network outputsz
+        
+        controller.release_all()
+        
+    except KeyboardInterrupt: #^C stops the keyboard
+        controller.stop()
+        print("\nStopped by user")
